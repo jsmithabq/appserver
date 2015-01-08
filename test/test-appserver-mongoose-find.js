281de03a420f505f5ece9c4f18e6d76902fa3847
Jerry Smith <jsmithabq@yahoo.com>
2015-01-08 09:25:57 -0700

    Added collection specification to schema.

--- test/test-appserver-mongoose-find.js
+++ test/test-appserver-mongoose-find.js
@@ -1,20 +1,20 @@
 
 var mongoose = require('mongoose');
-db = mongoose.createConnection('mongodb://appservertest:appservertest@ds029811.mongolab.com:29811/appserver');
+db = mongoose.createConnection(
+  'mongodb://appservertest:appservertest@ds029811.mongolab.com:29811/appserver');
 db.on('error', console.error.bind(console, 'connection error!'));
 db.once('open', function() {
-  var userSchema = new mongoose.Schema({
-    name: String,
-    role: String,
-    style: String
-  });
-  var User = db.model('user', userSchema);
+  var userSchema = new mongoose.Schema(
+    {name: String, role: String, style: String}, 
+    {collection: 'user'}
+  );
+  var User = db.model('users', userSchema);
   User.find(function(err, users) {
     if (err) {
-     console.log(err);
+     return console.log(err);
     } else {
-      mongoose.connection.close();
       console.log(users);
+      mongoose.connection.close();
       mongoose.disconnect();
     }
   });
