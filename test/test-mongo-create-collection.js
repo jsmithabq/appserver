0489a075eb119a1082b209b358cff8c2351ed3bc
Jerry Smith <jsmithabq@yahoo.com>
2015-01-09 11:26:00 -0700

    Improved argument diagnositcs.

--- test/test-mongo-create-collection.js
+++ test/test-mongo-create-collection.js
@@ -1,20 +1,23 @@
 
-var cmd_args = {};
-if (process.argv.length == 3) {
-  var args = process.argv.slice(2);
-  args.forEach(function (val, index, array) {
-    /*console.log(index + ': ' + val);*/
-    if (val.indexOf('=') != -1 && val.indexOf('=') == val.lastIndexOf('=')) {
-      var arg = val.split('=');
-      /*console.log('key = ' + arg[0]);
-      console.log('value = ' + arg[1]);*/
-      cmd_args[arg[0]] = arg[1];
-    } else {
-      console.log("Invalid argument: '" + val + "'.");
-      process.exit(1);
-    }
-  });
-  console.log(cmd_args);
+var cmdArgs = {};
+var args = process.argv.slice(2);
+args.forEach(function (val, index, array) {
+  /*console.log(index + ': ' + val);*/
+  if (val.indexOf('=') != -1 && val.indexOf('=') == val.lastIndexOf('=')) {
+    var arg = val.split('=');
+    /*console.log('key = ' + arg[0]);
+    console.log('value = ' + arg[1]);*/
+    cmdArgs[arg[0]] = arg[1];
+  } else {
+    console.log("Invalid argument: '" + val + "'");
+    process.exit(1);
+  }
+});
+console.log('Number of arguments = ' + Object.keys(cmdArgs).length);
+console.log(cmdArgs);
+if (Object.keys(cmdArgs).length != 1) {
+  console.log('Incorrect number of arguments (should be 1): ' + Object.keys(cmdArgs).length);
+  process.exit(1);
 }
 
 /*
@@ -26,25 +29,25 @@ var mc = mongo.MongoClient;
 mc.connect('mongodb://appservertest:appservertest@ds029811.mongolab.com:29811/appserver',
 function(err, db) {
   if (err) { console.log(err); return; }
-  if (cmd_args.collection == 'user' || cmd_args.collection == 'all') {
+  if (cmdArgs.collection == 'user' || cmdArgs.collection == 'all') {
     db.createCollection('user', {strict: true, capped: true, size: 50000, max: 50},
     function(err, collection) {
       if (err) { console.log(err); return; }
     });
   }
-  if (cmd_args.collection == 'profile' || cmd_args.collection == 'all') {
+  if (cmdArgs.collection == 'profile' || cmdArgs.collection == 'all') {
     db.createCollection('profile', {strict: true, capped: true, size: 50000, max: 20},
     function(err, collection) {
       if (err) { console.log(err); return; }
     });
   }
-  if (cmd_args.collection == 'africa' || cmd_args.collection == 'all') {
+  if (cmdArgs.collection == 'africa' || cmdArgs.collection == 'all') {
     db.createCollection('africa', {strict: true, capped: true, size: 50000, max: 60},
     function(err, collection) {
       if (err) { console.log(err); return; }
     });
   }
-  if (cmd_args.collection == 'australia' || cmd_args.collection == 'all') {
+  if (cmdArgs.collection == 'australia' || cmdArgs.collection == 'all') {
     db.createCollection('australia', {strict: true, capped: true, size: 50000, max: 10},
     function(err, collection) {
       if (err) { console.log(err); return; }
