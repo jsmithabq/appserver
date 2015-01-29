0f31a4a81a2ddd847c8069ebe435c39a849d6961
Jerry Smith <jsmithabq@yahoo.com>
2015-01-29 16:50:20 -0700

    Reading countries from database.

--- public/js/controllers/TabCtrl.js
+++ public/js/controllers/TabCtrl.js
@@ -1,17 +1,29 @@
 
-angular.module('TabCtrl', []).controller('TabController', function ($scope) {
+angular.module('TabCtrl', []).controller('TabController', function ($scope, $http) {
   $scope.placeholder = 'Placeholder...';
+  $scope.loadCountries = function() {
+    $http.get('api/africa/country').success (function (data) {
+      $scope.countries = [];
+      for (var i = 0; i < data.length; i++) {
+        $scope.countries.push(data[i].country);
+      }
+      console.log('Define countries...');
+      console.log('Countries: ', $scope.countries);
+    }).error(function () {
+      alert('Unexpected error retrieving countries.');
+    });
+  }
   var self = this;
   self.tab1 = {};
   self.tab1.distributor = 'Acme';
   self.tab1.region = 'West';
-  self.tab1.fruit = {
+  self.tab1.countriesOption = {
     'type': 'select', 
-    'label': 'Fruit:',
-    'name': 'fruit',
-    'value': 'Apple', 
-    'values': ['Apple', 'Banana', 'Peach', 'Strawberry']
+    'label': 'Countries:',
+    'name': 'country',
+    'value': 'Algeria', 
   };
+  self.tab1.countries = $scope.loadCountries();
   self.tab1_submit = function() {
     console.log('User clicked submit for ', self.tab1);
     console.log('User clicked submit with selection', self.tab1.fruit.value);
